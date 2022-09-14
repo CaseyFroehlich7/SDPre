ec3ef7eb00587b2f5a9443386d5da5dc86848db4
CaseyFroehlich <caseyfroehlich8559@gmail.com>
2022-09-14 11:59:41 -0400

    StringExample.java

---
+++ Labs/StringExample.java
@@ -0,0 +1,7 @@
+public class StringExample {
+  public static void main(String[] args) {
+    System.out.println("This is a sentence without quotes");
+    System.out.println("This is a sentence with \"quotes\".");
+
+  }
+}
