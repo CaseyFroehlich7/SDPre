9645d229d1d2499b1a48726cb098cc83071fce37
CaseyFroehlich <caseyfroehlich8559@gmail.com>
2022-09-15 11:00:52 -0400

    Objective2Lab2.java'

--- Labs/Objective2Lab2.java
+++ Labs/Objective2Lab2.java
@@ -3,7 +3,7 @@ public class Objective2Lab2 {
     String name = "Casey Froehlich";
     int age = 29;
     String state = "Pennsylvania";
-    System.out.println("Name: Casey Froehlich Age: 47 State: Pennsylvania");
+    System.out.println("Name:" + name  + " Age:" + age  + " State:" + state);
 
   }
 }
