c3e392b8d29b5ac35ecb0ef23efbebd8b067de7a
CaseyFroehlich <caseyfroehlich8559@gmail.com>
2022-09-20 12:43:13 -0400

    Objective3Lab2.java

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,11 @@
+public class Objective3Lab2 {
+  public static void main(String[] args) {
+    double side1 = 12;
+    double side2 = 5;
+    double result = Math.sqrt(side1 * side1 + side2 * side2);
+    System.out.println(result);
+    System.out.println("The hypotenuse of a triangle with sides 12.0 and 5.0" + " = " + result);
+  }
+
+
+}
