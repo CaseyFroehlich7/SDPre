3d564c4ccf827c3b47c380cbfea2db0b1a8e06a0
CaseyFroehlich <caseyfroehlich8559@gmail.com>
2022-10-05 14:22:46 -0400

    Objective3Lab1.java

--- Labs/Objective3Lab1.java
+++ Labs/Objective3Lab1.java
@@ -1,14 +1,14 @@
 public class Objective3Lab1 {
   public static void main(String[] args) {
-    int [] nums = {15, 6, 2};
-      float sum = 0;
-      int i=0;
-      while(i < nums.length) {
-        sum += nums[i];
-        i++;
-      }
-float average = (sum / nums.length);
-  System.out.println(average);
+
+    double num1, num2, num3, average;
+
+    num1 = 15;
+    num2 = 6;
+    num3 = 2;
+
+    average = (num1 + num2 + num3) / 3;
+    System.out.println(average);
 
   }
 }
